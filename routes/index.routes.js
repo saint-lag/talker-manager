e902bdd5913429848e2b9643fc176954663f0e46
saint-lag <maiasena.gabriel@gmail.com>
2022-07-02 14:09:51 -0300

    [PASS][REQ_2]

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,15 +5,30 @@ const router = Router();
 const FILE_PATH = './talker.json';
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 
 const { readFile } = require('../helpers/fs');
 
-router.get('/talker', (_req, res) => {
-  const parsedFile = readFile(FILE_PATH);
+router
+  .get('/talker', (_req, res) => {
+    const parsedFile = readFile(FILE_PATH);
 
-  if (!parsedFile) return res.status(HTTP_OK_STATUS).send([]);
+    if (!parsedFile) return res.status(HTTP_OK_STATUS).send([]);
 
-  return res.status(HTTP_OK_STATUS).json(parsedFile); 
-});
+    return res.status(HTTP_OK_STATUS).json(parsedFile);
+  })
+  .get('/talker/:id', (req, res) => {
+    const parsedFile = readFile(FILE_PATH);
+
+    const { id } = req.params;
+    const query = parsedFile.find((obj) => obj.id === Number(id));
+
+    if (!query || query === '') {
+      return res.status(HTTP_NOT_FOUND_STATUS).json({
+        message: 'Pessoa palestrante não encontrada',
+      });
+    }
+    return res.status(HTTP_OK_STATUS).json(query);
+  });
 
 module.exports = { router };
