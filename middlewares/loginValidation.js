672f812163064a800d172d66105b0904254eb651
saint-lag <maiasena.gabriel@gmail.com>
2022-07-03 12:46:08 -0300

    [PASS][REQ_4]

--- middlewares/loginValidation.js
+++ middlewares/loginValidation.js
@@ -1,22 +1,28 @@
+/* eslint-disable max-lines-per-function */
 const HTTP_BAD_REQUEST_STATUS = 400;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 function loginValidation(req, res, next) {
   const { email, password } = req.body;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const isValidEmail = emailRegex.test(email);
 
-  if (!email || !password) {
-    return res.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'Email ou senha não informados' });
-  }
+  if (!email) {
+ return res.status(HTTP_BAD_REQUEST_STATUS)
+  .json({ message: 'O campo "email" é obrigatório' }); 
+}
+  if (!password) {
+ return res.status(HTTP_BAD_REQUEST_STATUS)
+  .json({ message: 'O campo "password" é obrigatório' }); 
+}
   if (!isValidEmail) {
-    return res.status(HTTP_BAD_REQUEST_STATUS)
-    .json({ message: 'Formato permitido: example@email.com' });
-  }
+ return res.status(HTTP_BAD_REQUEST_STATUS)
+  .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+}
   if (password.length < 6) {
-    return res.status(HTTP_BAD_REQUEST_STATUS)
-    .json({ message: 'A senha de deve ter pelo menos 6 caracteres' });
-  }
-
+ return res.status(HTTP_BAD_REQUEST_STATUS)
+  .json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+}
+  
   next();
 }
 
